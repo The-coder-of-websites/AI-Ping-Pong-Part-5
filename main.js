9b4caa5db26062fa01312fff74c7209a551bc618
The-coder-of-websites <89913566+The-coder-of-websites@users.noreply.github.com>
2022-07-16 23:18:25 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -95,7 +95,7 @@ if(Game_status== "start") // inside the if condition check if the game_status is
     stroke(0,0,250);
     strokeWeight(0.5);
     paddle1Y = rightWristY; 
-    rect(paddle1X,paddle1Y,paddle1,paddle1Height,100);
+    rect(paddle1X,rightWristY,paddle1,paddle1Height,100);
 
 
     //pc computer paddle
@@ -168,11 +168,11 @@ function move(){
   if (ball.x-2.5*ball.r/2< 0){
   if (ball.y >= paddle1Y&& ball.y <= paddle1Y + paddle1Height) {
     ball.dx = -ball.dx+0.5; 
-    
+    ball_touch_paddel.play();
   }
   else{
     pcscore++;
-    
+    missed.play();
     reset();
     navigator.vibrate(100);
   }
@@ -185,7 +185,7 @@ if(pcscore ==4){
     stroke("white");
     textSize(25);
     text("Game Over!",width/2,height/2);
-    text("Reload the page!",width/2,height/2+30)
+    text("Press Restart Button To Play Again!",width/2,height/2+30)
     noLoop();
     pcscore = 0;
  }
@@ -216,5 +216,16 @@ function paddleInCanvas(){
   }
  
   
+}
+
+function preload(){
+  ball_touch_paddel=loadSound("ball_touch_paddel.wav");
+  missed=loadSound("missed.wav");
+}
+
+function restart(){
+  pcscore=0;
+  playerscore=0;
+  loop();
 }
 
